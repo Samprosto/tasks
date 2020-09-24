7c80bf640dd776b5427238791dc7a781f883a92d
Samprosto <32459817+Samprosto@users.noreply.github.com>
2020-09-24 17:41:54 +0300

    Delete auth.php

--- auth.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    if($_COOKIE['admin'] == 'admin')
-        setcookie('admin','admin',time()-3600,'/');
-    else
-        setcookie('admin','admin',time()+3600,'/');
-
-    header('Location: index_admin.php');
-?>
